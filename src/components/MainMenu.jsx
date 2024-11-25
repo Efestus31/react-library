e0fbf0430afe6caf9af96be869faf489960ab3cd
Efestus31 <biagiohd@gmail.com>
2024-11-25 11:03:35 +0100

    debug main menu

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -3,14 +3,18 @@ import { NavLink } from "react-router-dom";
 
 export default function MainMenu() {
 
-    <>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/books">Books</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/Contacts">Contacts</NavLink>
-        </nav>
-    </>
+
+    return (
+        <>
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/books">Books</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/Contacts">Contacts</NavLink>
+            </nav>
+        </>
+    )
+
 
 
 }
